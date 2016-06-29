2ed063bc696b560502b97edca5c621be660f9921
TheCodedOne <dr.kevincodex@instituteofgamers.co.uk>
2016-06-29 13:26:12 +0100

    IIntegration
    Added IIntegration
    
    Added Maven
    
    Removed CC & Travis

---
+++ src/main/java/com/acronym/base/integration/base/Base.java
@@ -0,0 +1,20 @@
+package com.acronym.base.integration.base;
+
+import com.acronym.base.integration.IIntegration;
+
+public class Base implements IIntegration {
+	@Override
+	public void preInit() {
+
+	}
+
+	@Override
+	public void init() {
+
+	}
+
+	@Override
+	public void postInit() {
+
+	}
+}
---
+++ src/main/java/com/acronym/base/integration/tesla/Tesla.java
@@ -0,0 +1,20 @@
+package com.acronym.base.integration.tesla;
+
+import com.acronym.base.integration.IIntegration;
+
+public class Tesla implements IIntegration {
+	@Override
+	public void preInit() {
+
+	}
+
+	@Override
+	public void init() {
+
+	}
+
+	@Override
+	public void postInit() {
+
+	}
+}
