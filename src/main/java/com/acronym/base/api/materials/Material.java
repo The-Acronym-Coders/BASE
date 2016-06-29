d41f2d51a3ed29ed6aa3dfeb460d09ff059f5f83
Jared <jaredlll08@gmail.com>
2016-06-29 21:47:28 +0200

    Moved Material
    Changed name to make more sense

--- src/main/java/com/acronym/base/api/materials/Material.java
+++ src/main/java/com/acronym/base/api/materials/Material.java
@@ -1,64 +1,49 @@
 package com.acronym.base.api.materials;
 
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by Jared on 6/27/2016.
- */
 public class Material {
 
     private String name;
-    private int colour;
-    private List<Resource> resources;
+    private Color color;
+    private EnumPartType[] types;
+    private List<EnumPartType> typeList = new ArrayList<>();
 
-    public Material(String name, int colour, List<Resource> resources) {
+    public Material(String name, Color color, EnumPartType... types) {
         this.name = name;
-        this.colour = colour;
-        this.resources = resources;
+        this.color = color;
+        this.types = types;
+        if (types != null)
+            for (EnumPartType type : types)
+                this.typeList.add(type);
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public Color getColor() {
+        return color;
     }
 
-    public int getColour() {
-        return colour;
+    public EnumPartType[] getTypes() {
+        return types;
     }
 
-    public void setColour(int colour) {
-        this.colour = colour;
+    public boolean isTypeSet(EnumPartType type) {
+        return this.typeList.contains(type);
     }
 
-    public List<Resource> getResources() {
-        return resources;
-    }
-
-    public void setResources(List<Resource> resources) {
-        this.resources = resources;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Material)) return false;
-
-        Material material = (Material) o;
-
-        if (getColour() != material.getColour()) return false;
-        if (getName() != null ? !getName().equals(material.getName()) : material.getName() != null) return false;
-        return getResources() != null ? getResources().equals(material.getResources()) : material.getResources() == null;
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = getName() != null ? getName().hashCode() : 0;
-        result = 31 * result + getColour();
-        result = 31 * result + (getResources() != null ? getResources().hashCode() : 0);
-        return result;
+    public enum EnumPartType {
+        NUGGET,
+        DUST,
+        INGOT,
+        BLOCK,
+        ORE,
+        GEAR,
+        FLUID,
+        PLATE
     }
 }
