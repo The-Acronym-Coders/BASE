e58f25eb1b9302666e814fa6a6c26368d7d97044
Jared <jaredlll08@gmail.com>
2016-06-30 11:30:30 +0200

    Removed copper and tin
    Added Iron and Gold

--- src/main/java/com/acronym/base/data/Materials.java
+++ src/main/java/com/acronym/base/data/Materials.java
@@ -1,16 +1,22 @@
-package com.acronym.base.data; 
- 
-import com.acronym.base.api.materials.Material; 
-import com.acronym.base.api.materials.Resource; 
- 
-import java.util.Arrays; 
- 
-/** 
- * Created by Jared on 6/27/2016. 
- */ 
-public class Materials { 
- 
-    public static final Material copper = new Material("Copper", 0xFF44FF, Arrays.asList(new Resource[]{new Resource("ingotCopper"), new Resource("dustCopper"), new Resource("oreCopper")})); 
-    public static final Material tin = new Material("Tin", 0x44FF44, Arrays.asList(new Resource[]{new Resource("ingotTin"), new Resource("dustTin"), new Resource("oreTin")})); 
- 
+package com.acronym.base.data;
+
+import com.acronym.base.api.materials.Material;
+import com.acronym.base.util.ColourHelper;
+import com.acronym.base.util.ResourceUtils;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+
+import java.awt.*;
+
+/**
+ * Created by Jared on 6/27/2016.
+ */
+public class Materials {
+
+//    public static final Material copper = new Material("Copper", 0xFF44FF, Arrays.asList(new Resource[]{new Resource("ingotCopper"), new Resource("dustCopper"), new Resource("oreCopper")}));
+//    public static final Material tin = new Material("Tin", 0x44FF44, Arrays.asList(new Resource[]{new Resource("ingotTin"), new Resource("dustTin"), new Resource("oreTin")}));
+
+    public static final Material iron = new Material("Iron", new Color(ColourHelper.getColour(ResourceUtils.getResourceFromItem(new ItemStack(Items.IRON_INGOT)).getInputStream())), Material.EnumPartType.values());
+    public static final Material gold = new Material("Gold", new Color(ColourHelper.getColour(ResourceUtils.getResourceFromItem(new ItemStack(Items.GOLD_INGOT)).getInputStream())), Material.EnumPartType.values());
+    
 } 
