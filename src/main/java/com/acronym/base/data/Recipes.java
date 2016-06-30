387ca69f9ed4ed6f4c0c080d763d0f1473e5ccef
Jared <jaredlll08@gmail.com>
2016-06-30 11:53:48 +0200

    Removed auto gear gen.
    Made it easier to add materials, only having to create a field

--- src/main/java/com/acronym/base/data/Recipes.java
+++ src/main/java/com/acronym/base/data/Recipes.java
@@ -1,15 +1,9 @@
 package com.acronym.base.data;
 
 import com.acronym.base.api.materials.Material;
-import com.acronym.base.api.materials.Resource;
 import com.acronym.base.api.materials.registries.MaterialRegistry;
-import com.acronym.base.util.ColourHelper;
-import com.acronym.base.util.ResourceUtils;
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.oredict.OreDictionary;
 
-import java.util.Arrays;
-import java.util.List;
+import java.lang.reflect.Field;
 
 /**
  * Created by Jared on 6/27/2016.
@@ -21,22 +15,29 @@ public class Recipes {
     }
 
     public static void init() {
-        MaterialRegistry.addMaterial("Copper", Materials.copper);
-        MaterialRegistry.addMaterial("Tin", Materials.tin);
-        for (String s : OreDictionary.getOreNames()) {
-            if (s.startsWith("ingot") && !OreDictionary.getOres("ore" + s.substring(5)).isEmpty()) {
-                String name = s.substring(5);
-                List<ItemStack> list = OreDictionary.getOres(s);
-                for (ItemStack item : list) {
-                    try {
-                        int colour = ColourHelper.getColour(ResourceUtils.getResourceFromItem(item).getInputStream());
-                        MaterialRegistry.addMaterial(name, new Material(name, colour, Arrays.asList(new Resource[]{new Resource(String.format("ingot%s", name)), new Resource(String.format("dust%s", name)), new Resource(String.format("ore%s", name))})));
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
+        for (Field f : Materials.class.getDeclaredFields()) {
+            try {
+                Material mat = new Material();
+                mat = (Material) f.get(mat);
+                MaterialRegistry.addMaterial(mat.getName().toLowerCase(), mat);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
             }
         }
+//        for (String s : OreDictionary.getOreNames()) {
+//            if (s.startsWith("ingot") && !OreDictionary.getOres("ore" + s.substring(5)).isEmpty()) {
+//                String name = s.substring(5);
+//                List<ItemStack> list = OreDictionary.getOres(s);
+//                for (ItemStack item : list) {
+//                    try {
+//                        int colour = ColourHelper.getColour(ResourceUtils.getResourceFromItem(item).getInputStream());
+//                        MaterialRegistry.addMaterial(name, new Material(name, colour, Arrays.asList(new Resource[]{new Resource(String.format("ingot%s", name)), new Resource(String.format("dust%s", name)), new Resource(String.format("ore%s", name))})));
+//                    } catch (Exception e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
+//        }
     }
 
     public static void postInit() {
