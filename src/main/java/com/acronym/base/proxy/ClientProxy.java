ce8b0ab5b90270f06f329f87c7bc8e4bd43fe68b
Jared <jaredlll08@gmail.com>
2016-07-02 00:27:04 +0200

    Added lasers/lines

--- src/main/java/com/acronym/base/proxy/ClientProxy.java
+++ src/main/java/com/acronym/base/proxy/ClientProxy.java
@@ -1,13 +1,16 @@
 package com.acronym.base.proxy;
 
+import com.acronym.base.blocks.BlockTest;
+import com.acronym.base.client.render.RenderTileEntityTest;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.client.FMLClientHandler;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 
 public class ClientProxy extends CommonProxy {
 
     public void registerRenderers() {
-
+        ClientRegistry.registerTileEntity(BlockTest.TileEntityTest.class, "testRender", new RenderTileEntityTest());
     }
 
 
