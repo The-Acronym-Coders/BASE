550c47da31809cd01ae7b51ee494f006eda73dc8
EwyBoy <ewyboy@gmail.com>
2016-07-03 03:46:57 +0200

    Versioning update
    Updated version string and added acceptedMinecraftVersions array

--- src/main/java/com/acronym/base/reference/Reference.java
+++ src/main/java/com/acronym/base/reference/Reference.java
@@ -4,7 +4,13 @@ public class Reference {
  
     public final static String MODID = "base";
     public final static String NAME = "B.A.S.E";
-    public final static String VERSION = "0.0.0";
+    public static final String MINECRAFT_VERSION = "[1.9.4]";
+
+    static final String VERSION_MAJOR = "0";
+    static final String VERSION_MINOR = "0";
+    static final String VERSION_PATCH = "0";
+
+    public static final String BUILD_VERSION = VERSION_MAJOR + "." + VERSION_MINOR + "." + VERSION_PATCH + "-" + MINECRAFT_VERSION;
 
     public final static TabBase tab = new TabBase();
 } 
