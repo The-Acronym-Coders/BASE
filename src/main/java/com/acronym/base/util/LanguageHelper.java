680010d064a6638f8901a47d688c358e5e5e5e1a
Jared <jaredlll08@gmail.com>
2016-07-05 02:47:40 +0200

    Documented LanguageHelper

--- src/main/java/com/acronym/base/util/LanguageHelper.java
+++ src/main/java/com/acronym/base/util/LanguageHelper.java
@@ -5,33 +5,125 @@ import net.minecraft.client.resources.I18n;
 public class LanguageHelper {
     private String modid;
 
+    /**
+     * Initializes a new instance of LanguageHelper
+     *
+     * @param modid
+     */
     public LanguageHelper(String modid) {
         this.modid = modid;
     }
 
+    /**
+     * Translates a message
+     *
+     * @param label   prefix
+     * @param message message
+     * @return Translated String
+     */
     public String translateMessage(String label, String message) {
-        if(label=="") return I18n.format(message);
+        if (label == "") return I18n.format(message);
         return I18n.format(String.format("%s.%s.%s", label, modid, message));
     }
 
-    public String none(String message) { return translateMessage("",message); }
+    /**
+     * Returns an emptry translated string
+     *
+     * @param message message
+     * @return Translated String
+     */
+    public String none(String message) {
+        return translateMessage("", message);
+    }
 
-    public String message(String message) { return translateMessage("message",message); }
+    /**
+     * Translates a message
+     *
+     * @param message message
+     * @returnTranslated String
+     */
+    public String message(String message) {
+        return translateMessage("message", message);
+    }
 
-    public String label(String message) { return translateMessage("label",message); }
+    /**
+     * Translates a message
+     *
+     * @param message message
+     * @returnTranslated String
+     */
+    public String label(String message) {
+        return translateMessage("label", message);
+    }
 
-    public String block(String message) { return translateMessage("block",message); }
+    /**
+     * Translates a message
+     *
+     * @param message message
+     * @returnTranslated String
+     */
+    public String block(String message) {
+        return translateMessage("block", message);
+    }
 
-    public String item(String message) { return translateMessage("item",message); }
+    /**
+     * Translates a message
+     *
+     * @param message message
+     * @returnTranslated String
+     */
+    public String item(String message) {
+        return translateMessage("item", message);
+    }
 
-    public String itemgroup(String message) { return translateMessage("itemGroup",message); }
+    /**
+     * Translates a message
+     *
+     * @param message message
+     * @returnTranslated String
+     */
+    public String itemgroup(String message) {
+        return translateMessage("itemGroup", message);
+    }
 
-    public String description(String message) { return translateMessage("description",message); }
+    /**
+     * Translates a message
+     *
+     * @param message message
+     * @returnTranslated String
+     */
+    public String description(String message) {
+        return translateMessage("description", message);
+    }
 
-    public String jei(String message) { return translateMessage("jei",message); }
+    /**
+     * Translates a message
+     *
+     * @param message message
+     * @returnTranslated String
+     */
+    public String jei(String message) {
+        return translateMessage("jei", message);
+    }
 
-    public String material(String message) { return translateMessage("material",message); }
+    /**
+     * Translates a message
+     *
+     * @param message message
+     * @returnTranslated String
+     */
+    public String material(String message) {
+        return translateMessage("material", message);
+    }
 
-    public String error(String message) { return translateMessage("error",message); }
+    /**
+     * Translates a message
+     *
+     * @param message message
+     * @returnTranslated String
+     */
+    public String error(String message) {
+        return translateMessage("error", message);
+    }
 
 }
