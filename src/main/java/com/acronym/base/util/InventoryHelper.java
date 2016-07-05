a59a75f1e6afdbc291647017441ccbe164ae70b5
Jared <jaredlll08@gmail.com>
2016-07-05 02:47:30 +0200

    Documented InventoryHelper

--- src/main/java/com/acronym/base/util/InventoryHelper.java
+++ src/main/java/com/acronym/base/util/InventoryHelper.java
@@ -4,18 +4,45 @@ import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 
 public class InventoryHelper {
+    /**
+     * Adds an ItemStack to an inventory
+     *
+     * @param itemIn    ItemStack to add
+     * @param inventory Inventory to add to
+     * @param slotStart startSlot
+     * @param slotEnd   endSlot
+     * @return ItemStack
+     */
     public static ItemStack addItemStackToInventory(ItemStack itemIn, IInventory inventory, int slotStart, int slotEnd) {
         return addItemStackToInventory(itemIn, inventory, slotStart, slotEnd, false);
     }
 
+    /**
+     * Checks if the output is full
+     *
+     * @param inventory Inventory to check
+     * @param slotStart StartSlot
+     * @param slotEnd   EndSlot
+     * @return boolean showing weither the output is full
+     */
     public static boolean isOutputFull(IInventory inventory, int slotStart, int slotEnd) {
         boolean full = true;
-        for(int i=slotStart;i<slotEnd;i++) {
+        for (int i = slotStart; i < slotEnd; i++) {
             full = !(inventory.getStackInSlot(i) == null && full);
         }
         return full;
     }
 
+    /**
+     * Adds an ItemStack to an inventory
+     *
+     * @param itemIn    ItemStack to add
+     * @param inventory Inventory to add to
+     * @param slotStart startSlot
+     * @param slotEnd   endSlot
+     * @param simulate  should the action actually be done?
+     * @return Itemstack
+     */
     public static ItemStack addItemStackToInventory(ItemStack itemIn, IInventory inventory, int slotStart, int slotEnd, boolean simulate) {
         if (itemIn == null) return null;
         ItemStack itemOut = itemIn.copy();
