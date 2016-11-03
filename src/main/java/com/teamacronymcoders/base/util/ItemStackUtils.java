c5881c5fadacc18ffd64d0c293969af3bfa9436b
Skylar Sommers <ssommers@criticalmix.com>
2016-11-03 13:46:09 -0400

    Added IsSmeltable to ItemStackUtils

--- src/main/java/com/teamacronymcoders/base/util/ItemStackUtils.java
+++ src/main/java/com/teamacronymcoders/base/util/ItemStackUtils.java
@@ -2,9 +2,15 @@ package com.teamacronymcoders.base.util;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.FurnaceRecipes;
 
 public class ItemStackUtils {
-    private ItemStackUtils() { }
+    private ItemStackUtils() {
+    }
+
+    public static boolean isSmeltable(ItemStack itemStack) {
+        return itemStack != null && FurnaceRecipes.instance().getSmeltingResult(itemStack) != null;
+    }
 
     public static boolean isItemInstanceOf(ItemStack itemStack, Class itemClass) {
         return itemStack != null && itemClass != null && itemClass.isInstance(itemStack.getItem());
