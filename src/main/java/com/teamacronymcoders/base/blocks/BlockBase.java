6adcb9c98fc7d53b3482406dd6c7e16b63cbec74
Skylar Sommers <ssommers@criticalmix.com>
2016-11-03 11:41:20 -0400

    Added BlockBase

---
+++ src/main/java/com/teamacronymcoders/base/blocks/BlockBase.java
@@ -0,0 +1,78 @@
+package com.teamacronymcoders.base.blocks;
+
+import com.teamacronymcoders.base.IBaseMod;
+import com.teamacronymcoders.base.client.models.IHasModel;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+
+public class BlockBase extends Block implements IHasItemBlock, IHasModel {
+    IBaseMod mod;
+    boolean creativeTabSet = false;
+
+    public BlockBase(Material mat) {
+        super(mat);
+        this.setHardness(1F);
+    }
+
+    public BlockBase(Material mat, String name) {
+        this(mat);
+        this.setUnlocalizedName(name);
+    }
+
+    @Override
+    public void breakBlock(@Nonnull World world, @Nonnull BlockPos pos, @Nonnull IBlockState state) {
+        world.updateComparatorOutputLevel(pos, this);
+
+        super.breakBlock(world, pos, state);
+    }
+
+    @Override
+    public void onBlockAdded(World world, BlockPos pos, IBlockState state) {
+        this.updateState(world, pos, state);
+        super.onBlockAdded(world, pos, state);
+    }
+
+    @Override
+    public void onNeighborChange(IBlockAccess world, BlockPos pos, BlockPos neighborPos) {
+        this.updateState(world, pos, world.getBlockState(neighborPos));
+        super.onNeighborChange(world, pos, neighborPos);
+    }
+
+    protected void updateState(IBlockAccess world, BlockPos pos, IBlockState state) {
+
+    }
+
+    @Override
+    @Nonnull
+    public Block setCreativeTab(@Nonnull CreativeTabs tab) {
+        if (!creativeTabSet) {
+            super.setCreativeTab(tab);
+            this.creativeTabSet = true;
+        }
+        return this;
+    }
+
+    @Override
+    public ItemBlock getItemBlock() {
+        return new ItemBlock(this);
+    }
+
+    @Override
+    public List<String> getModelNames(List<String> modelNames) {
+        String name = this.getUnlocalizedName();
+        if (name.startsWith("tile.")) {
+            name = name.substring(5);
+        }
+        modelNames.add(name);
+        return modelNames;
+    }
+}
