58c7eb997a014238f97c847d9116d44ce73a0513
Skylar Sommers <ssommers@criticalmix.com>
2016-11-03 13:45:59 -0400

    Made EnumUtils static

--- src/main/java/com/teamacronymcoders/base/util/EnumUtils.java
+++ src/main/java/com/teamacronymcoders/base/util/EnumUtils.java
@@ -3,7 +3,9 @@ package com.teamacronymcoders.base.util;
 import java.util.Arrays;
 
 public class EnumUtils {
-    public String[] getNames(Class<? extends Enum<?>> enumClass) {
+    private EnumUtils() { }
+
+    public static String[] getNames(Class<? extends Enum<?>> enumClass) {
         return Arrays.stream(enumClass.getEnumConstants()).map(Enum::name).toArray(String[]::new);
     }
 }
