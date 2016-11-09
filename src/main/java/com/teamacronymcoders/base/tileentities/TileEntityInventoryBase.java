af83fdae77d3d6fed78a41f26fee069d7ee703d7
Jared <jaredlll08@gmail.com>
2016-11-10 00:07:59 +0200

    Not sure why this didn't write to NBT before...

--- src/main/java/com/teamacronymcoders/base/tileentities/TileEntityInventoryBase.java
+++ src/main/java/com/teamacronymcoders/base/tileentities/TileEntityInventoryBase.java
@@ -1,23 +1,36 @@
 package com.teamacronymcoders.base.tileentities;
 
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.items.CapabilityItemHandler;
-import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 public class TileEntityInventoryBase extends TileEntityBase {
-    private IItemHandler inventory;
+    private ItemStackHandler inventory;
 
     public TileEntityInventoryBase(int inventorySize) {
         this(new ItemStackHandler(inventorySize));
     }
 
-    public TileEntityInventoryBase(IItemHandler inventory) {
+    public TileEntityInventoryBase(ItemStackHandler inventory) {
         this.inventory = inventory;
+    }
+
+
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound data) {
+        data.setTag("Items", inventory.serializeNBT());
+        return super.writeToNBT(data);
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound data) {
+        super.readFromNBT(data);
+        inventory.deserializeNBT(data.getCompoundTag("Items"));
     }
 
     @Override
