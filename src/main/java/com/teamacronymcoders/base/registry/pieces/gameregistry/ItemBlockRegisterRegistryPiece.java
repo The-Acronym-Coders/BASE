fe284f89e2a0e2949b7a00e2847490d362fddc53
Skylar Sommers <ssommers@criticalmix.com>
2016-12-26 19:38:32 -0500

    ItemBlockPiece now pulls Registry during set mod

--- src/main/java/com/teamacronymcoders/base/registry/pieces/gameregistry/ItemBlockRegisterRegistryPiece.java
+++ src/main/java/com/teamacronymcoders/base/registry/pieces/gameregistry/ItemBlockRegisterRegistryPiece.java
@@ -1,22 +1,20 @@
 package com.teamacronymcoders.base.registry.pieces.gameregistry;
 
+import com.teamacronymcoders.base.IBaseMod;
 import com.teamacronymcoders.base.blocks.IHasItemBlock;
+import com.teamacronymcoders.base.registry.ItemRegistry;
 import com.teamacronymcoders.base.registry.Registry;
 import com.teamacronymcoders.base.registry.pieces.RegistryPiece;
 import com.teamacronymcoders.base.registry.pieces.RegistryPieceBase;
-import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
 
 @RegistryPiece
 public class ItemBlockRegisterRegistryPiece extends RegistryPieceBase<IHasItemBlock> {
-    private Registry<Item> itemRegistry;
+    private ItemRegistry itemRegistry;
 
     @Override
     @SuppressWarnings("unchecked")
     public boolean acceptsRegistry(Registry registry) {
-        if("ITEM".equalsIgnoreCase(registry.getName())) {
-            itemRegistry = registry;
-        }
         return "BLOCK".equalsIgnoreCase(registry.getName());
     }
 
@@ -28,5 +26,11 @@ public class ItemBlockRegisterRegistryPiece extends RegistryPieceBase<IHasItemBl
     @Override
     public void addEntry(ResourceLocation name, IHasItemBlock block) {
         itemRegistry.register(name, block.getItemBlock());
+    }
+
+    @Override
+    public void setMod(IBaseMod mod) {
+        super.setMod(mod);
+        this.itemRegistry = mod.getRegistryHolder().getRegistry(ItemRegistry.class, "ITEM");
     }
 }
