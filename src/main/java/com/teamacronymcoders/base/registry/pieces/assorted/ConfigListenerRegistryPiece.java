80f3c4ae42ca9770b7d6367b7ee52168fb6576a9
Skylar Sommers <ssommers@criticalmix.com>
2016-12-26 20:18:35 -0500

    Moved getting the registry, since it's not there during setmod

--- src/main/java/com/teamacronymcoders/base/registry/pieces/assorted/ConfigListenerRegistryPiece.java
+++ src/main/java/com/teamacronymcoders/base/registry/pieces/assorted/ConfigListenerRegistryPiece.java
@@ -1,6 +1,5 @@
 package com.teamacronymcoders.base.registry.pieces.assorted;
 
-import com.teamacronymcoders.base.IBaseMod;
 import com.teamacronymcoders.base.registry.config.ConfigRegistry;
 import com.teamacronymcoders.base.registry.config.IConfigListener;
 import com.teamacronymcoders.base.registry.pieces.RegistryPiece;
@@ -18,12 +17,9 @@ public class ConfigListenerRegistryPiece extends RegistryPieceBase<IConfigListen
 
     @Override
     public void addEntry(ResourceLocation name, IConfigListener listener) {
+        if(configRegistry == null) {
+            configRegistry = this.getMod().getRegistryHolder().getRegistry(ConfigRegistry.class, "CONFIG");
+        }
         configRegistry.addListener(listener);
-    }
-
-    @Override
-    public void setMod(IBaseMod mod) {
-        super.setMod(mod);
-        this.configRegistry = mod.getRegistryHolder().getRegistry(ConfigRegistry.class, "CONFIG");
     }
 }
--- src/main/java/com/teamacronymcoders/base/registry/pieces/gameregistry/ItemBlockRegisterRegistryPiece.java
+++ src/main/java/com/teamacronymcoders/base/registry/pieces/gameregistry/ItemBlockRegisterRegistryPiece.java
@@ -1,6 +1,5 @@
 package com.teamacronymcoders.base.registry.pieces.gameregistry;
 
-import com.teamacronymcoders.base.IBaseMod;
 import com.teamacronymcoders.base.blocks.IHasItemBlock;
 import com.teamacronymcoders.base.registry.ItemRegistry;
 import com.teamacronymcoders.base.registry.Registry;
@@ -23,12 +22,9 @@ public class ItemBlockRegisterRegistryPiece extends RegistryPieceBase<IHasItemBl
 
     @Override
     public void addEntry(ResourceLocation name, IHasItemBlock block) {
+        if(itemRegistry == null) {
+            this.itemRegistry = this.getMod().getRegistryHolder().getRegistry(ItemRegistry.class, "ITEM");
+        }
         itemRegistry.register(name, block.getItemBlock());
-    }
-
-    @Override
-    public void setMod(IBaseMod mod) {
-        super.setMod(mod);
-        this.itemRegistry = mod.getRegistryHolder().getRegistry(ItemRegistry.class, "ITEM");
     }
 }
