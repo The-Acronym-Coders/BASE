1502bd1df68601dde12a8251cd849b68c0e01c19
Skylar Sommers <ssommers@criticalmix.com>
2016-12-26 19:56:27 -0500

    Removed unneeded stuff

--- src/main/java/com/teamacronymcoders/base/registry/Registry.java
+++ src/main/java/com/teamacronymcoders/base/registry/Registry.java
@@ -1,17 +1,8 @@
 package com.teamacronymcoders.base.registry;
 
 import com.teamacronymcoders.base.IBaseMod;
-import com.teamacronymcoders.base.IModAware;
-import com.teamacronymcoders.base.items.IHasOreDict;
-import com.teamacronymcoders.base.items.IHasRecipe;
-import com.teamacronymcoders.base.registry.config.ConfigRegistry;
-import com.teamacronymcoders.base.registry.config.IConfigListener;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.oredict.OreDictionary;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,56 +19,15 @@ public abstract class Registry<T> {
     }
 
     public void preInit() {
-        this.entries.forEach((name, entry) -> {
-            initiateEntry(name, entry);
-            initiateModel(name, entry);
-        });
-
         setLoadingStage(LoadingStage.INIT);
     }
 
     public void init() {
-        this.entries.forEach((name, entry) -> {
-            initiateRecipes(entry);
-            initiateOreDict(entry);
-            initiateColor(entry);
-        });
-
         setLoadingStage(LoadingStage.POSTINIT);
     }
 
     public void postInit() {
         setLoadingStage(LoadingStage.DONE);
-    }
-
-    protected void initiateEntry(ResourceLocation name, T entry) {
-        if (entry instanceof IConfigListener) {
-            mod.getRegistryHolder().getRegistry(ConfigRegistry.class, "CONFIG").addListener((IConfigListener) entry);
-        }
-        if (entry instanceof IModAware) {
-            ((IModAware) entry).setMod(mod);
-        }
-    }
-
-    protected void initiateModel(ResourceLocation name, T entry) {
-
-    }
-
-    protected void initiateColor(T entry) {
-
-    }
-
-    protected void initiateOreDict(T entry) {
-        if(entry instanceof IHasOreDict) {
-            Map<ItemStack, String> oreDict = ((IHasOreDict) entry).getOreDictNames(new HashMap<>());
-            oreDict.forEach((itemStack, s) -> OreDictionary.registerOre(s, itemStack));
-        }
-    }
-
-    public void initiateRecipes(T entry) {
-        if (entry instanceof IHasRecipe) {
-            ((IHasRecipe) entry).getRecipes(new ArrayList<>()).forEach(GameRegistry::addRecipe);
-        }
     }
 
     public T get(String name) {
