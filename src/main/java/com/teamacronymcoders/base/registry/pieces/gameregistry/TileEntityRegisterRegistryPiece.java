3f321dedcd70e2d592319f7f85ab47e671e8dedd
Skylar Sommers <ssommers@criticalmix.com>
2016-12-26 19:08:36 -0500

    Added TileEntityRegister Registry Piece

--- src/main/java/com/teamacronymcoders/base/registry/pieces/gameregistry/TileEntityRegisterRegistryPiece.java
+++ src/main/java/com/teamacronymcoders/base/registry/pieces/gameregistry/TileEntityRegisterRegistryPiece.java
@@ -1,14 +1,14 @@
 package com.teamacronymcoders.base.registry.pieces.gameregistry;
 
+import com.teamacronymcoders.base.blocks.IHasTileEntity;
 import com.teamacronymcoders.base.registry.Registry;
 import com.teamacronymcoders.base.registry.pieces.RegistryPiece;
 import com.teamacronymcoders.base.registry.pieces.RegistryPieceBase;
-import net.minecraft.block.Block;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @RegistryPiece
-public class BlockRegisterRegistryPiece extends RegistryPieceBase<Block> {
+public class TileEntityRegisterRegistryPiece extends RegistryPieceBase<IHasTileEntity> {
     @Override
     public boolean acceptsRegistry(Registry registry) {
         return "BLOCK".equalsIgnoreCase(registry.getName());
@@ -16,12 +16,11 @@ public class BlockRegisterRegistryPiece extends RegistryPieceBase<Block> {
 
     @Override
     public boolean acceptsEntry(ResourceLocation name, Object entry) {
-        return entry instanceof Block;
+        return entry instanceof IHasTileEntity;
     }
 
     @Override
-    public void preInit(ResourceLocation name, Block entry) {
-        entry.setCreativeTab(this.getMod().getCreativeTab());
-        GameRegistry.register(entry, name);
+    public void preInit(ResourceLocation name, IHasTileEntity entry) {
+        GameRegistry.registerTileEntity(entry.getTileEntityClass(), name.toString());
     }
 }
