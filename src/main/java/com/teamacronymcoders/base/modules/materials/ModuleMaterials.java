b713b97c67c8ded94f4b1fed9178867f75c02fbb
Skylar Sommers <ssommers@criticalmix.com>
2017-01-08 00:43:16 -0500

    BASE Now only has a Creative Tab if it has Materials Registered

--- src/main/java/com/teamacronymcoders/base/modules/materials/ModuleMaterials.java
+++ src/main/java/com/teamacronymcoders/base/modules/materials/ModuleMaterials.java
@@ -4,6 +4,7 @@ import com.teamacronymcoders.base.Base;
 import com.teamacronymcoders.base.api.materials.MaterialRegistry;
 import com.teamacronymcoders.base.api.materials.MaterialType;
 import com.teamacronymcoders.base.api.materials.MaterialType.EnumPartType;
+import com.teamacronymcoders.base.creativetabs.CreativeTabCarousel;
 import com.teamacronymcoders.base.modules.materials.blocks.BlockMaterial;
 import com.teamacronymcoders.base.modules.materials.blocks.BlockProperties;
 import com.teamacronymcoders.base.modules.materials.items.ItemPart;
@@ -16,6 +17,7 @@ import com.teamacronymcoders.base.registry.config.ConfigEntry;
 import com.teamacronymcoders.base.registry.config.ConfigRegistry;
 import net.minecraftforge.common.config.Property;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -82,10 +84,19 @@ public class ModuleMaterials extends ModuleBase {
     }
 
     @Override
+    public void preInit(FMLPreInitializationEvent event) {
+        if(MaterialRegistry.getMaterials().size() > 0) {
+            CreativeTabCarousel tabCarousel = new CreativeTabCarousel("base");
+            tabCarousel.setIconStacks(GEAR.getAllSubItems(new ArrayList<>()));
+            Base.instance.setCreativeTab(tabCarousel);
+        }
+        super.preInit(event);
+    }
+
+    @Override
     public void init(FMLInitializationEvent event) {
         super.init(event);
         MaterialRecipes.init();
-        Base.tabCarousel.setIconStacks(GEAR.getAllSubItems(new ArrayList<>()));
     }
 
     public static class MaterialConfigEntry extends ConfigEntry {
