f9fc462a70b5abc79a2cf255b3a60273c119f9d8
Skylar Sommers <ssommers@criticalmix.com>
2017-01-09 23:29:28 -0500

    Fixed BlockSubBase not implementing getSubItems

--- src/main/java/com/teamacronymcoders/base/blocks/BlockSubBase.java
+++ src/main/java/com/teamacronymcoders/base/blocks/BlockSubBase.java
@@ -3,6 +3,9 @@ package com.teamacronymcoders.base.blocks;
 import com.teamacronymcoders.base.items.ItemSubBlock;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.ItemStack;
+
+import java.util.List;
 
 public class BlockSubBase extends BlockBase {
     String[] names;
@@ -10,7 +13,15 @@ public class BlockSubBase extends BlockBase {
     public BlockSubBase(Material mat, String[] names) {
         super(mat);
         this.names = names;
-        setItemBlock(new ItemSubBlock(this, names));
+        setItemBlock(new ItemSubBlock<>(this, names));
+    }
+
+    @Override
+    public List<ItemStack> getAllSubItems(List<ItemStack> itemStacks) {
+        for(int x = 0; x < names.length; x++) {
+            itemStacks.add(new ItemStack(this, 1, x));
+        }
+        return itemStacks;
     }
 
     @Override
