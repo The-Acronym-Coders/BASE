0af495135a84ff637fa7de3b1f06b118946e8ba4
warlordjones <warlordjones@gmail.com>
2017-01-14 16:13:10 +0000

    Start moving some more old stuff from boilerplate.

---
+++ src/main/java/com/teamacronymcoders/base/tileentities/IOnSlotChanged.java
@@ -0,0 +1,10 @@
+package com.teamacronymcoders.base.tileentities;
+
+import net.minecraft.inventory.Slot;
+
+/**
+ * Created by Skylar on 8/31/2015.
+ */
+public interface IOnSlotChanged {
+	void onSlotChanged(Slot slot);
+}
---
+++ src/main/java/com/teamacronymcoders/base/util/Utils.java
@@ -0,0 +1,58 @@
+package com.teamacronymcoders.base.util;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.world.World;
+
+public class Utils {
+
+	/*
+	 * Everything below this line is #StolenByTheBrassGoggledCoders from ZeroCore. Temporarily!
+	 * TODO
+	 */
+
+	public static int getChunkXFromBlock(int blockX) {
+		return blockX >> 4;
+	}
+
+	public static int getChunkXFromBlock(BlockPos position) {
+		return position.getX() >> 4;
+	}
+
+	public static int getChunkZFromBlock(int blockZ) {
+		return blockZ >> 4;
+	}
+
+	public static int getChunkZFromBlock(BlockPos position) {
+		return position.getZ() >> 4;
+	}
+
+	/**
+	 * force a block update at the given position
+	 * 
+	 * @param world the world to update
+	 * @param position the position of the block begin updated
+	 * @param oldState the old state of the block begin updated. if null, the current state will be retrieved from the
+	 *            world
+	 * @param newState the new state for the block begin updated. if null, the final value of oldState will be used
+	 */
+	public static void notifyBlockUpdate(World world, BlockPos position, IBlockState oldState, IBlockState newState) {
+
+		if(null == oldState)
+			oldState = world.getBlockState(position);
+
+		if(null == newState)
+			newState = oldState;
+
+		world.notifyBlockUpdate(position, oldState, newState, 3);
+	}
+
+	public static long getChunkXZHashFromBlock(int blockX, int blockZ) {
+		return ChunkPos.chunkXZ2Int(getChunkXFromBlock(blockX), getChunkZFromBlock(blockZ));
+	}
+
+	public static long getChunkXZHashFromBlock(BlockPos position) {
+		return ChunkPos.chunkXZ2Int(getChunkXFromBlock(position), getChunkZFromBlock(position));
+	}
+}
