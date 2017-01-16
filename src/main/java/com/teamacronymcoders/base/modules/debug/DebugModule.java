51488df75e3d64cdd94243666046adc57cae65cd
Skylar Sommers <ssommers@criticalmix.com>
2017-01-15 23:15:55 -0500

    Added Debugger Stick back

---
+++ src/main/java/com/teamacronymcoders/base/modules/debug/DebugModule.java
@@ -0,0 +1,24 @@
+package com.teamacronymcoders.base.modules.debug;
+
+import com.teamacronymcoders.base.modules.debug.items.ItemDebuggerStick;
+import com.teamacronymcoders.base.modulesystem.ModuleBase;
+import com.teamacronymcoders.base.registry.ItemRegistry;
+import com.teamacronymcoders.base.registry.config.ConfigRegistry;
+import com.teamacronymcoders.base.util.Platform;
+
+public class DebugModule extends ModuleBase{
+    @Override
+    public String getName() {
+        return "Debug";
+    }
+
+    @Override
+    public boolean getActiveDefault() {
+        return Platform.isDevEnv();
+    }
+
+    @Override
+    public void registerItems(ConfigRegistry configRegistry, ItemRegistry itemRegistry) {
+        itemRegistry.register(new ItemDebuggerStick());
+    }
+}
