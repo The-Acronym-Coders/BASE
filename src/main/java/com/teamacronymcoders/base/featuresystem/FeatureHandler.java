7ca12cdd1883d31221035ea3dba49c36c7fe0cb6
Skylar Sommers <ssommers@criticalmix.com>
2017-02-06 19:55:42 -0500

    Added isFeatureActivated to Feature Handler

--- src/main/java/com/teamacronymcoders/base/featuresystem/FeatureHandler.java
+++ src/main/java/com/teamacronymcoders/base/featuresystem/FeatureHandler.java
@@ -7,18 +7,24 @@ public class FeatureHandler {
     private static List<String> requests = new ArrayList<>();
 
     public static void registerFeature(String name, IFeature feature) {
-        name = name.toLowerCase(Locale.ROOT);
+        name = name.toLowerCase(Locale.US);
         features.put(name, feature);
         attemptActivate(name);
     }
 
     public static void requestFeature(String name) {
-        name = name.toLowerCase(Locale.ROOT);
+        name = name.toLowerCase(Locale.US);
         if(features.containsKey(name)) {
             activate(name);
         } else {
             requests.add(name);
         }
+    }
+
+    public static boolean isFeatureActivated(String name) {
+        name = name.toLowerCase(Locale.US);
+        IFeature feature = features.get(name);
+        return feature != null && feature.isActive();
     }
 
     private static void attemptActivate(String name) {
