01ec7ddb5532f90fb72cca8aa56575854f74e398
Skylar Sommers <ssommers@criticalmix.com>
2017-02-10 09:10:35 -0500

    Beginning of EntityMaterialGolem

---
+++ src/main/java/com/teamacronymcoders/base/materialsystem/MaterialPartDataSerializer.java
@@ -0,0 +1,32 @@
+package com.teamacronymcoders.base.materialsystem;
+
+import com.teamacronymcoders.base.entities.dataserializers.ResourceLocationDataSerializer;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializer;
+import net.minecraft.util.ResourceLocation;
+
+import javax.annotation.Nonnull;
+import java.io.IOException;
+
+public class MaterialPartDataSerializer implements DataSerializer<MaterialPart> {
+    @Override
+    public void write(@Nonnull PacketBuffer buf, @Nonnull MaterialPart value) {
+        buf.writeString(value.getRegistryName().getResourceDomain());
+        buf.writeString(value.getRegistryName().getResourcePath());
+    }
+
+    @Override
+    @Nonnull
+    public MaterialPart read(@Nonnull PacketBuffer buf) throws IOException {
+        ResourceLocation resourceLocation = new ResourceLocation(buf.readStringFromBuffer(256),
+                buf.readStringFromBuffer(256));
+        return MaterialsSystem.MATERIAL_PARTS.getValue(resourceLocation);
+    }
+
+    @Override
+    @Nonnull
+    public DataParameter<MaterialPart> createKey(int id) {
+        return new DataParameter<>(id, this);
+    }
+}
