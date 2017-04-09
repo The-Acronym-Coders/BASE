53dfbaf913624b23acb13ee7e1b65c69d8994a8f
Skylar Sommers <ssommers@criticalmix.com>
2017-04-09 16:43:36 -0400

    Added Poor Ore to V2

--- src/main/java/com/teamacronymcoders/base/materialsystem/parts/ProvidedParts.java
+++ src/main/java/com/teamacronymcoders/base/materialsystem/parts/ProvidedParts.java
@@ -2,14 +2,13 @@ package com.teamacronymcoders.base.materialsystem.parts;
 
 import com.teamacronymcoders.base.materialsystem.MaterialsSystem;
 import com.teamacronymcoders.base.materialsystem.blocks.SubBlockPart;
+import com.teamacronymcoders.base.materialsystem.materials.Material;
 import com.teamacronymcoders.base.subblocksystem.SubBlockSystem;
 
 public class ProvidedParts {
     public static final PartType ITEM = new PartType("Item");
     public static final PartType BLOCK = new PartType("Block", materialPart ->
             SubBlockSystem.registerSubBlock(new SubBlockPart(materialPart)));
-    public static final PartType ORE = new PartType("ORE", materialPart -> {
-    });
 
     public static final Part INGOT = new Part("Ingot", ITEM);
     public static final Part BEAM = new Part("Beam", ITEM);
@@ -19,12 +18,12 @@ public class ProvidedParts {
     public static final Part PLATE = new Part("Plate", ITEM);
     public static final Part NUGGET = new Part("Nugget", ITEM);
     public static final Part STORAGE = new Part("Storage", BLOCK);
-    public static final Part ORE_BLOCK = new Part("Ore", ORE);
+    public static final Part ORE_BLOCK = new Part("Ore", BLOCK);
+    public static final Part POOR_ORE_BLOCK = new Part("Poor Ore", BLOCK);
 
     public static void initPartTypes() {
         MaterialsSystem.registerPartType(ITEM);
         MaterialsSystem.registerPartType(BLOCK);
-        MaterialsSystem.registerPartType(ORE);
     }
 
     public static void initParts() {
@@ -38,7 +37,6 @@ public class ProvidedParts {
 
         MaterialsSystem.registerPart(STORAGE);
         MaterialsSystem.registerPart(ORE_BLOCK);
+        MaterialsSystem.registerPart(POOR_ORE_BLOCK);
     }
-
-
 }
