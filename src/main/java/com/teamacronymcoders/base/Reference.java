5eb734fdf19d92b8eb866a981e9c1bed1135b660
Jared <jaredlll08@gmail.com>
2017-07-09 15:36:48 +0200

    Update Reference.java

--- src/main/java/com/teamacronymcoders/base/Reference.java
+++ src/main/java/com/teamacronymcoders/base/Reference.java
@@ -3,7 +3,7 @@ package com.teamacronymcoders.base;
 public class Reference {
     public static final String MODID = "base";
     public static final String NAME = "BASE";
-    public static final String DEPENDENCIES = "after:MineTweaker3;after:jei";
+    public static final String DEPENDENCIES = "after:crafttweaker;after:jei";
     public static final String VERSION = "@VERSION@";
 
     private Reference() {
