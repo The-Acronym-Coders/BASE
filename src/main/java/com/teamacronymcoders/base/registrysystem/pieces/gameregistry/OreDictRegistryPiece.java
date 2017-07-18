5e93aceebcd23099c900a05c0d0ab97957af9e0e
warlordjones <warlordjones@gmail.com>
2017-07-18 17:27:39 +0100

    Do oredict registration in init not preinit so the registered items exist.

--- src/main/java/com/teamacronymcoders/base/registrysystem/pieces/gameregistry/OreDictRegistryPiece.java
+++ src/main/java/com/teamacronymcoders/base/registrysystem/pieces/gameregistry/OreDictRegistryPiece.java
@@ -1,26 +1,27 @@
 package com.teamacronymcoders.base.registrysystem.pieces.gameregistry;
 
-import com.teamacronymcoders.base.items.IHasOreDict;
-import com.teamacronymcoders.base.registrysystem.pieces.RegistryPiece;
-import com.teamacronymcoders.base.registrysystem.pieces.RegistryPieceBase;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.oredict.OreDictionary;
+import static net.minecraftforge.fml.common.eventhandler.EventPriority.LOW;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static net.minecraftforge.fml.common.eventhandler.EventPriority.LOW;
+import com.teamacronymcoders.base.items.IHasOreDict;
+import com.teamacronymcoders.base.registrysystem.pieces.RegistryPiece;
+import com.teamacronymcoders.base.registrysystem.pieces.RegistryPieceBase;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.oredict.OreDictionary;
 
 @RegistryPiece(priority = LOW)
 public class OreDictRegistryPiece extends RegistryPieceBase<IHasOreDict> {
-    public OreDictRegistryPiece() {
-        super(IHasOreDict.class);
-    }
+	public OreDictRegistryPiece() {
+		super(IHasOreDict.class);
+	}
 
-    @Override
-    public void preInit(ResourceLocation name, IHasOreDict entry) {
-        Map<ItemStack, String> oreDict = entry.getOreDictNames(new HashMap<>());
-        oreDict.forEach((itemStack, string) -> OreDictionary.registerOre(string, itemStack));
-    }
+	@Override
+	public void init(ResourceLocation name, IHasOreDict entry) {
+		Map<ItemStack, String> oreDict = entry.getOreDictNames(new HashMap<>());
+		oreDict.forEach((itemStack, string) -> OreDictionary.registerOre(string, itemStack));
+	}
 }
