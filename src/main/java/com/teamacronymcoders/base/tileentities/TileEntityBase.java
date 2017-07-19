9d99a96fb20ccb9ee6b6b8b82eafb82efa0a2624
warlordjones <warlordjones@gmail.com>
2017-07-19 17:00:47 +0100

    Remove forced multiblock syncing through generic update packets
    Also abstract NBT disk callbacks and update logger in affected files.

--- src/main/java/com/teamacronymcoders/base/tileentities/TileEntityBase.java
+++ src/main/java/com/teamacronymcoders/base/tileentities/TileEntityBase.java
@@ -1,14 +1,9 @@
 package com.teamacronymcoders.base.tileentities;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.network.NetworkManager;
-import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 /**
  * @author SkySom
@@ -19,8 +14,8 @@ public abstract class TileEntityBase extends TileEntity {
 
 	@Override
 	public void readFromNBT(NBTTagCompound data) {
-		super.readFromNBT(data);
 		this.readFromDisk(data);
+		super.readFromNBT(data);
 	}
 
 	@Override
@@ -30,46 +25,9 @@ public abstract class TileEntityBase extends TileEntity {
 		return super.writeToNBT(data);
 	}
 
-	protected void readFromDisk(NBTTagCompound data) {}
+	protected abstract void readFromDisk(NBTTagCompound data);
 
-	protected NBTTagCompound writeToDisk(NBTTagCompound data) {
-		return data;
-	}
-
-	@Override
-	public void handleUpdateTag(@Nonnull NBTTagCompound data) {
-		super.readFromNBT(data);
-		this.readFromUpdatePacket(data);
-	}
-
-	@Override
-	@Nonnull
-	public NBTTagCompound getUpdateTag() {
-		NBTTagCompound data = super.getUpdateTag();
-		this.writeToUpdatePacket(data);
-		return super.getUpdateTag();
-	}
-
-	@Override
-	@SideOnly(Side.CLIENT)
-	public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity packet) {
-		this.readFromUpdatePacket(packet.getNbtCompound());
-		super.onDataPacket(net, packet);
-	}
-
-	@Nullable
-	@Override
-	public SPacketUpdateTileEntity getUpdatePacket() {
-		NBTTagCompound data = new NBTTagCompound();
-		this.writeToUpdatePacket(data);
-		return new SPacketUpdateTileEntity(this.getPos(), 0, data);
-	}
-
-	protected void readFromUpdatePacket(NBTTagCompound data) {}
-
-	protected NBTTagCompound writeToUpdatePacket(NBTTagCompound data) {
-		return data;
-	}
+	protected abstract NBTTagCompound writeToDisk(NBTTagCompound data);
 
 	public void sendBlockUpdate() {
 		if(!this.getWorld().isRemote) {
