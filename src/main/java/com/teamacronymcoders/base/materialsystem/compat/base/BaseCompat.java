9bb18eb60aa6f2b79159950c79fcb88e433e25b0
Skylar Sommers <ssommers@criticalmix.com>
2017-08-20 19:57:01 -0400

    Switched Base to new ParBuilder

--- src/main/java/com/teamacronymcoders/base/materialsystem/compat/base/BaseCompat.java
+++ src/main/java/com/teamacronymcoders/base/materialsystem/compat/base/BaseCompat.java
@@ -3,6 +3,7 @@ package com.teamacronymcoders.base.materialsystem.compat.base;
 import com.teamacronymcoders.base.Base;
 import com.teamacronymcoders.base.materialsystem.MaterialException;
 import com.teamacronymcoders.base.materialsystem.MaterialSystem;
+import com.teamacronymcoders.base.materialsystem.MaterialUser;
 import com.teamacronymcoders.base.materialsystem.compat.IMaterialCompat;
 import com.teamacronymcoders.base.materialsystem.compat.MaterialCompat;
 import com.teamacronymcoders.base.materialsystem.parts.PartBuilder;
@@ -12,6 +13,8 @@ import com.teamacronymcoders.base.materialsystem.parttype.*;
 public class BaseCompat implements IMaterialCompat {
     @Override
     public void doCompat() throws MaterialException {
+        MaterialUser user = Base.instance.getMaterialUser();
+        
         PartType item = new ItemPartType();
         PartType block = new BlockPartType();
         PartType ore = new OrePartType();
@@ -22,23 +25,23 @@ public class BaseCompat implements IMaterialCompat {
         MaterialSystem.registerPartType(ore);
         MaterialSystem.registerPartType(fluid);
 
-        registerPart(new PartBuilder().setName("Ingot").setPartType(item));
-        registerPart(new PartBuilder().setName("Beam").setPartType(item));
-        registerPart(new PartBuilder().setName("Gear").setPartType(item));
-        registerPart(new PartBuilder().setName("Bolt").setPartType(item));
-        registerPart(new PartBuilder().setName("Dust").setPartType(item));
-        registerPart(new PartBuilder().setName("Nugget").setPartType(item));
-        registerPart(new PartBuilder().setName("Rod").setPartType(item));
-        registerPart(new PartBuilder().setName("Plate").setPartType(item));
-        registerPart(new PartBuilder().setName("Dense Plate").setPartType(item));
+        registerPart(new PartBuilder(user).setName("Ingot").setPartType(item));
+        registerPart(new PartBuilder(user).setName("Beam").setPartType(item));
+        registerPart(new PartBuilder(user).setName("Gear").setPartType(item));
+        registerPart(new PartBuilder(user).setName("Bolt").setPartType(item));
+        registerPart(new PartBuilder(user).setName("Dust").setPartType(item));
+        registerPart(new PartBuilder(user).setName("Nugget").setPartType(item));
+        registerPart(new PartBuilder(user).setName("Rod").setPartType(item));
+        registerPart(new PartBuilder(user).setName("Plate").setPartType(item));
+        registerPart(new PartBuilder(user).setName("Dense Plate").setPartType(item));
 
-        registerPart(new PartBuilder().setName("Block").setPartType(block));
+        registerPart(new PartBuilder(user).setName("Block").setPartType(block));
 
-        registerPart(new PartBuilder().setName("Ore").setPartType(ore));
-        registerPart(new PartBuilder().setName("Poor Ore").setPartType(ore));
-        registerPart(new PartBuilder().setName("Dense Ore").setPartType(ore));
+        registerPart(new PartBuilder(user).setName("Ore").setPartType(ore));
+        registerPart(new PartBuilder(user).setName("Poor Ore").setPartType(ore));
+        registerPart(new PartBuilder(user).setName("Dense Ore").setPartType(ore));
 
-        registerPart(new PartBuilder().setName("Fluid").setPartType(fluid));
+        registerPart(new PartBuilder(user).setName("Fluid").setPartType(fluid));
     }
 
     private void registerPart(PartBuilder partBuilder) {
