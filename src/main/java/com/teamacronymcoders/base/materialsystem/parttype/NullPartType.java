08e93e8546561dd20d6e14b75c2218a165986f54
Skylar Sommers <ssommers@criticalmix.com>
2017-08-26 16:35:50 -0400

    Parts now return the proper item stack
    Will need this for the MaterialPart bracket handler

--- src/main/java/com/teamacronymcoders/base/materialsystem/parttype/NullPartType.java
+++ src/main/java/com/teamacronymcoders/base/materialsystem/parttype/NullPartType.java
@@ -1,7 +1,15 @@
 package com.teamacronymcoders.base.materialsystem.parttype;
 
+import com.teamacronymcoders.base.materialsystem.materialparts.MaterialPart;
+import net.minecraft.item.ItemStack;
+
 public class NullPartType extends PartType {
     public NullPartType() {
         super("NULL");
+    }
+
+    @Override
+    public ItemStack getItemStack(MaterialPart materialPart) {
+        return ItemStack.EMPTY;
     }
 }
