bf6a80399dd1a344edfa6dbd2d34f18e4a2d2aca
SkySom <skylar.som@gmail.com>
2017-10-11 23:34:46 -0400

    Fix lang entries by removing spaces

--- src/main/java/com/teamacronymcoders/base/util/files/ResourceLoader.java
+++ src/main/java/com/teamacronymcoders/base/util/files/ResourceLoader.java
@@ -1,16 +1,25 @@
 package com.teamacronymcoders.base.util.files;
 
+import com.google.common.collect.Lists;
+import com.teamacronymcoders.base.Base;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.IResourcePack;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.relauncher.ReflectionHelper;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.nio.charset.MalformedInputException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @SideOnly(Side.CLIENT)
 public class ResourceLoader {
@@ -34,6 +43,34 @@ public class ResourceLoader {
         }
 
         createPackMcMeta();
+        prepareResources();
+    }
+
+    private void prepareResources() {
+        fixLangFolder();
+    }
+
+    private void fixLangFolder() {
+        File[] modFolders = this.resourceFolder.listFiles(File::isDirectory);
+        if (Objects.nonNull(modFolders)) {
+            Arrays.stream(modFolders)
+                    .map(modFolder -> new File(modFolder, "lang"))
+                    .filter(File::exists)
+                    .filter(File::isDirectory)
+                    .map(File::listFiles)
+                    .filter(Objects::nonNull)
+                    .flatMap(Arrays::stream)
+                    .forEach(this::fixLangFile);
+        }
+
+    }
+
+    Pattern pattern = Pattern.compile(" += +");
+
+    private void fixLangFile(File file) {
+        String fileString = BaseFileUtils.readFileToString(file);
+        fileString = pattern.matcher(fileString).replaceAll("=");
+        BaseFileUtils.writeStringToFile(fileString, file);
     }
 
     private void createPackMcMeta() {
