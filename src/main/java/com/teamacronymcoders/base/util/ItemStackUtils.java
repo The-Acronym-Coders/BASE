8dccd1657c8de382fed095818f90eb605f4f551a
SkySom <skylar.som@gmail.com>
2017-11-24 21:08:30 -0500

    Added isPlayerCarrying

--- src/main/java/com/teamacronymcoders/base/util/ItemStackUtils.java
+++ src/main/java/com/teamacronymcoders/base/util/ItemStackUtils.java
@@ -1,12 +1,13 @@
 package com.teamacronymcoders.base.util;
 
-import javax.annotation.Nonnull;
-
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.FurnaceRecipes;
+
+import javax.annotation.Nonnull;
 
 public class ItemStackUtils {
     private ItemStackUtils() {
@@ -35,6 +36,11 @@ public class ItemStackUtils {
             itemStack = new ItemStack(item, 1, blockState.getBlock().getMetaFromState(blockState));
         }
         return itemStack;
+    }
+
+    public static boolean isPlayerCarrying(Item item, EntityPlayer player) {
+        return ItemStackUtils.doItemsMatch(player.getHeldItemMainhand(), item)
+                || ItemStackUtils.doItemsMatch(player.getHeldItemOffhand(), item);
     }
 
     @Nonnull
