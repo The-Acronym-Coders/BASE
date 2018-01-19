ac0ce143dcb6770ef3e88e05d7cd62fc4e3f0c1b
Justin Russo <j.russo@outlook.com>
2018-01-18 23:59:57 -0500

    Allow for drops data to be set on ore sample

--- src/main/java/com/teamacronymcoders/base/materialsystem/blocks/SubBlockOreSamplePart.java
+++ src/main/java/com/teamacronymcoders/base/materialsystem/blocks/SubBlockOreSamplePart.java
@@ -1,22 +1,78 @@
 package com.teamacronymcoders.base.materialsystem.blocks;
 
 import com.google.common.collect.Maps;
+import com.teamacronymcoders.base.IBaseMod;
 import com.teamacronymcoders.base.client.models.generator.generatedmodel.GeneratedModel;
 import com.teamacronymcoders.base.client.models.generator.generatedmodel.IGeneratedModel;
 import com.teamacronymcoders.base.client.models.generator.generatedmodel.ModelType;
 import com.teamacronymcoders.base.materialsystem.MaterialSystem;
+import com.teamacronymcoders.base.materialsystem.MaterialUser;
 import com.teamacronymcoders.base.materialsystem.materialparts.MaterialPart;
+import com.teamacronymcoders.base.materialsystem.partdata.MaterialPartData;
 import com.teamacronymcoders.base.materialsystem.parts.Part;
+import com.teamacronymcoders.base.util.ItemStackUtils;
+import com.teamacronymcoders.base.util.OreDictUtils;
 import com.teamacronymcoders.base.util.files.templates.TemplateFile;
 import com.teamacronymcoders.base.util.files.templates.TemplateManager;
 import net.minecraft.block.state.BlockFaceShape;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
+import java.util.List;
 import java.util.Map;
 
+import static com.teamacronymcoders.base.materialsystem.parttype.OreSamplePartType.DROP_DATA_NAME;
+
 public class SubBlockOreSamplePart extends SubBlockPart {
-    public SubBlockOreSamplePart(MaterialPart materialPart) {
+    private ItemStack itemStackToDrop = null;
+    private String itemDrop;
+    private IBaseMod mod;
+
+    public SubBlockOreSamplePart(MaterialPart materialPart, MaterialUser materialUser) {
         super(materialPart, MaterialSystem.materialCreativeTab);
+        MaterialPartData data = materialPart.getData();
+        this.mod = materialUser.getMod();
+        if (data.containsDataPiece(DROP_DATA_NAME)) {
+            itemDrop = data.getDataPiece(DROP_DATA_NAME);
+        }
+    }
+
+    @Override
+    public void getDrops(int fortune, List<ItemStack> itemStacks) {
+        if (itemStackToDrop == null) {
+            if (itemDrop != null && !itemDrop.isEmpty()) {
+                String[] itemDropArray = itemDrop.split(":");
+                String itemString = itemDropArray[0];
+
+                if (itemString.equalsIgnoreCase("oredict")) {
+                    itemStackToDrop = OreDictUtils.getPreferredItemStack(itemDropArray[1]);
+                } else {
+                    int meta = 0;
+                    if (itemDropArray.length > 1) {
+                        itemString += ":" + itemDropArray[1];
+                        if (itemDropArray.length > 2) {
+                            meta = Integer.parseInt(itemDropArray[2]);
+                        }
+                    }
+                    Item item = ForgeRegistries.ITEMS.getValue(new ResourceLocation(itemString));
+                    if (item != null) {
+                        itemStackToDrop = new ItemStack(item, 1, meta);
+                    } else {
+                        this.mod.getLogger().error("Could not find Item for name: " + itemString);
+                    }
+                }
+            } else {
+                this.itemStackToDrop = this.getItemStack();
+            }
+        }
+        if (ItemStackUtils.isValid(this.itemStackToDrop)) {
+            itemStacks.add(itemStackToDrop.copy());
+        } else {
+            mod.getLogger().fatal("Couldn't drop null ItemStack for " + getMaterialPart().getLocalizedName());
+        }
     }
 
     @Override
--- src/main/java/com/teamacronymcoders/base/materialsystem/parttype/OreSamplePartType.java
+++ src/main/java/com/teamacronymcoders/base/materialsystem/parttype/OreSamplePartType.java
@@ -1,18 +1,28 @@
 package com.teamacronymcoders.base.materialsystem.parttype;
 
+import com.google.common.collect.Lists;
 import com.teamacronymcoders.base.materialsystem.MaterialUser;
 import com.teamacronymcoders.base.materialsystem.blocks.SubBlockOreSamplePart;
 import com.teamacronymcoders.base.materialsystem.materialparts.MaterialPart;
 
 import javax.annotation.Nonnull;
+import java.util.List;
 
 public class OreSamplePartType extends BlockPartType {
+    public static final String DROP_DATA_NAME = "drops";
+
     public OreSamplePartType() {
-        super("Ore Sample");
+        super("Ore Sample", setupOreSampleData());
+    }
+
+    private static List<PartDataPiece> setupOreSampleData() {
+        List<PartDataPiece> oreDataPieces = Lists.newArrayList();
+        oreDataPieces.add(new PartDataPiece(DROP_DATA_NAME, false));
+        return oreDataPieces;
     }
 
     @Override
     public void setup(@Nonnull MaterialPart materialPart, @Nonnull MaterialUser materialUser) {
-        registerSubBlock(materialPart, new SubBlockOreSamplePart(materialPart));
+        registerSubBlock(materialPart, new SubBlockOreSamplePart(materialPart, materialUser));
     }
 }
