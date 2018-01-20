93a163e6b1eed24fc5155891c9e00dff88755969
SkySom <skylar.som@gmail.com>
2018-01-20 02:33:34 -0500

    Gui Handler tweaks

--- src/main/java/com/teamacronymcoders/base/guisystem/GuiHandler.java
+++ src/main/java/com/teamacronymcoders/base/guisystem/GuiHandler.java
@@ -6,6 +6,10 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.network.IGuiHandler;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.Optional;
 
 public class GuiHandler implements IGuiHandler {
     public GuiHandler(IBaseMod instance) {
@@ -15,18 +19,20 @@ public class GuiHandler implements IGuiHandler {
     @Override
     public Object getServerGuiElement(int id, EntityPlayer player, World world, int x, int y, int z) {
         BlockPos blockPos = new BlockPos(x, y, z);
-        IHasGui openableGUI = this.getHasGUI(id, player, world, blockPos);
-        return openableGUI != null ? openableGUI.getContainer(player, world, blockPos) : null;
+        return this.getHasGUI(id, player, world, blockPos)
+                .map(openableGUI -> openableGUI.getContainer(player, world, blockPos))
+                .orElse(null);
     }
 
     @Override
     public Object getClientGuiElement(int id, EntityPlayer player, World world, int x, int y, final int z) {
         BlockPos blockPos = new BlockPos(x, y, z);
-        IHasGui openableGUI = this.getHasGUI(id, player, world, blockPos);
-        return openableGUI != null ? openableGUI.getGui(player, world, blockPos) : null;
+        return this.getHasGUI(id, player, world, blockPos)
+                .map(openableGUI -> openableGUI.getGui(player, world, blockPos))
+                .orElse(null);
     }
 
-    private IHasGui getHasGUI(int id, EntityPlayer player, World world, BlockPos blockPos) {
-        return GuiCarrier.values()[id].getHasGUI(player, world, blockPos);
+    private Optional<IHasGui> getHasGUI(int id, EntityPlayer player, World world, BlockPos blockPos) {
+        return Optional.ofNullable(GuiCarrier.values()[id].getHasGUI(player, world, blockPos));
     }
 }
