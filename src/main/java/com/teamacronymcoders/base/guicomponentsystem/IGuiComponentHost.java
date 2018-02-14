5bef5f69455d751f9826c8e3866eb174bb3fac60
SkySom <skylar.som@gmail.com>
2018-02-14 00:09:03 -0500

    Remove old gui stuff.

--- src/main/java/com/teamacronymcoders/base/guicomponentsystem/IGuiComponentHost.java
+++
@@ -1,8 +0,0 @@
-package com.teamacronymcoders.base.guicomponentsystem;
-
-import com.teamacronymcoders.base.guisystem.IHasGui;
-import net.minecraft.util.math.BlockPos;
-
-public interface IGuiComponentHost extends IHasGui {
-    BlockPos getLocation();
-}
--- src/main/java/com/teamacronymcoders/base/guicomponentsystem/container/package-info.java
+++
@@ -1,4 +0,0 @@
-@ParametersAreNonnullByDefault
-package com.teamacronymcoders.base.guicomponentsystem.container;
-
-import javax.annotation.ParametersAreNonnullByDefault;
