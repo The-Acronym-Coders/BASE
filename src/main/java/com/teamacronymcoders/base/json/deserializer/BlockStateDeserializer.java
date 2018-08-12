c8ca0328d1cfb12cea4cf723d69f44956c9c7bfb
SkySom <skylar.som@gmail.com>
2018-08-12 16:30:24 -0400

    Alloow BlockState Deserializer to complain at nulls

--- src/main/java/com/teamacronymcoders/base/json/deserializer/BlockStateDeserializer.java
+++ src/main/java/com/teamacronymcoders/base/json/deserializer/BlockStateDeserializer.java
@@ -15,29 +15,35 @@ import java.util.Map;
 public class BlockStateDeserializer implements JsonDeserializer<IBlockState> {
     @Override
     public IBlockState deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-        JsonObject jsonObject = json.getAsJsonObject();
-        String blockName = JsonUtils.getString(jsonObject,"block");
-        Block block = ForgeRegistries.BLOCKS.getValue(new ResourceLocation(blockName));
-        if (block != null) {
-            if (jsonObject.has("properties")) {
-                BlockStateContainer blockStateContainer = block.getBlockState();
-                IBlockState blockState = block.getDefaultState();
-                JsonObject properties = jsonObject.getAsJsonObject("properties");
-                for (Map.Entry<String, JsonElement> entry : properties.entrySet()) {
-                    IProperty<?> property = blockStateContainer.getProperty(entry.getKey());
-                    if (property != null) {
-                        blockState = setValueHelper(blockState, property, entry.getValue().getAsString());
-                    } else {
-                        throw new JsonParseException("Failed to find property: " + entry.getKey());
+        if (json != null && json.isJsonObject()) {
+            JsonObject jsonObject = json.getAsJsonObject();
+            String blockName = JsonUtils.getString(jsonObject, "block");
+            Block block = ForgeRegistries.BLOCKS.getValue(new ResourceLocation(blockName));
+            if (block != null) {
+                if (jsonObject.has("properties")) {
+                    BlockStateContainer blockStateContainer = block.getBlockState();
+                    IBlockState blockState = block.getDefaultState();
+                    JsonObject properties = jsonObject.getAsJsonObject("properties");
+                    for (Map.Entry<String, JsonElement> entry : properties.entrySet()) {
+                        IProperty<?> property = blockStateContainer.getProperty(entry.getKey());
+                        if (property != null) {
+                            blockState = setValueHelper(blockState, property, entry.getValue().getAsString());
+                        } else {
+                            throw new JsonParseException("Failed to find property: " + entry.getKey());
+                        }
                     }
+                    return blockState;
+                } else {
+                    return block.getDefaultState();
                 }
-                return blockState;
-            } else {
-                return block.getDefaultState();
             }
         }
 
-        throw new JsonParseException("Failed to deserialize BlockState for: " + json.toString());
+        if (json == null) {
+            throw new JsonParseException("Found null value for BlockState Input");
+        } else {
+            throw new JsonParseException("Failed to deserialize BlockState for: " + json.toString());
+        }
     }
 
     private static <T extends Comparable<T>> IBlockState setValueHelper(final IBlockState blockState, final IProperty<T> property, final String stringValue) throws JsonParseException {
