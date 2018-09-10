bbf535cbd6127524a1072d6217d1a1ba418390ad
SkySom <skylar.som@gmail.com>
2018-09-10 18:28:16 -0400

    Add inBiome check to Biome

--- src/main/java/com/teamacronymcoders/base/recipesystem/condition/BiomeCondition.java
+++ src/main/java/com/teamacronymcoders/base/recipesystem/condition/BiomeCondition.java
@@ -10,14 +10,16 @@ import javax.annotation.Nullable;
 public class BiomeCondition implements ICondition {
     @JsonAdapter(value = ResourceLocation.Serializer.class, nullSafe = false)
     private final ResourceLocation biome;
+    private final boolean inBiome;
 
-    public BiomeCondition(ResourceLocation biome) {
+    public BiomeCondition(ResourceLocation biome, boolean inBiome) {
         this.biome = biome;
+        this.inBiome = inBiome;
     }
 
     @Override
     public boolean isMet(RecipeContainer recipeContainer, @Nullable EntityPlayer entityPlayer) {
         return recipeContainer.getWorld().isBlockLoaded(recipeContainer.getPos()) &&
-                biome.equals(recipeContainer.getWorld().getBiome(recipeContainer.getPos()).getRegistryName());
+                (biome.equals(recipeContainer.getWorld().getBiome(recipeContainer.getPos()).getRegistryName()) == inBiome);
     }
 }
