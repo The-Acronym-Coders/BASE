acfd66c9058577d287e529ff381473fcdbc69d25
SkySom <skylar.som@gmail.com>
2019-01-23 21:10:55 -0500

    Finish NBTDeserializer

--- src/main/java/com/teamacronymcoders/base/json/deserializer/NBTDeserializer.java
+++ src/main/java/com/teamacronymcoders/base/json/deserializer/NBTDeserializer.java
@@ -1,7 +1,10 @@
 package com.teamacronymcoders.base.json.deserializer;
 
 import com.google.gson.*;
+import com.google.gson.annotations.JsonAdapter;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.nbt.NBTTagLongArray;
 
 import java.lang.reflect.Type;
 import java.util.Map;
@@ -18,7 +21,7 @@ public class NBTDeserializer implements JsonDeserializer<NBTTagCompound> {
     private NBTTagCompound parseObject(JsonObject json) {
         NBTTagCompound tagCompound = new NBTTagCompound();
 
-        for (Map.Entry<String, JsonElement> property: json.entrySet()) {
+        for (Map.Entry<String, JsonElement> property : json.entrySet()) {
             String name = property.getKey();
             JsonElement jsonElement = property.getValue();
             if (jsonElement.isJsonObject()) {
@@ -56,6 +59,67 @@ public class NBTDeserializer implements JsonDeserializer<NBTTagCompound> {
     }
 
     private void handleJsonArray(NBTTagCompound tagCompound, String propertyName, JsonArray jsonArray) {
+        boolean allLongs = true;
+        boolean allInts = true;
+        boolean allBytes = true;
 
+        for (JsonElement element : jsonArray) {
+            if (!element.isJsonPrimitive()) {
+                throw new JsonParseException("Only Longs, Ints, and Bytes are supported in NBT Arrays");
+            } else {
+                JsonPrimitive primitive = element.getAsJsonPrimitive();
+                if (primitive.isNumber()) {
+                    Number number = primitive.getAsNumber();
+                    if (number instanceof Byte) {
+                        allLongs = false;
+                        allInts = false;
+                    } else if (number instanceof Integer) {
+                        allLongs = false;
+                        allBytes = false;
+                    } else if (number instanceof Long) {
+                        allInts = false;
+                        allBytes = false;
+                    } else {
+                        throw new JsonParseException("Only Longs, Ints, and Bytes are supported in NBT Arrays");
+                    }
+                } else {
+                    throw new JsonParseException("Only Longs, Ints, and Bytes are supported in NBT Arrays");
+                }
+            }
+        }
+
+        if (allLongs) {
+            handleJsonLongArray(tagCompound, propertyName, jsonArray);
+        } else if (allInts) {
+            handleJsonIntArray(tagCompound, propertyName, jsonArray);
+        } else if (allBytes) {
+            handleJsonByteArray(tagCompound, propertyName, jsonArray);
+        } else {
+            throw new JsonParseException("NBT Arrays must all be the Same Type");
+        }
+    }
+
+    private void handleJsonByteArray(NBTTagCompound tagCompound, String propertyName, JsonArray jsonArray) {
+        byte[] byteArray = new byte[jsonArray.size()];
+        for (int i = 0; i < jsonArray.size(); i++) {
+            byteArray[i] = jsonArray.get(i).getAsJsonPrimitive().getAsByte();
+        }
+        tagCompound.setByteArray(propertyName, byteArray);
+    }
+
+    private void handleJsonIntArray(NBTTagCompound tagCompound, String propertyName, JsonArray jsonArray) {
+        int[] intArray = new int[jsonArray.size()];
+        for (int i = 0; i < jsonArray.size(); i++) {
+            intArray[i] = jsonArray.get(i).getAsJsonPrimitive().getAsInt();
+        }
+        tagCompound.setIntArray(propertyName, intArray);
+    }
+
+    private void handleJsonLongArray(NBTTagCompound tagCompound, String propertyName, JsonArray jsonArray) {
+        long[] longArray = new long[jsonArray.size()];
+        for (int i = 0; i < jsonArray.size(); i++) {
+            longArray[i] = jsonArray.get(i).getAsJsonPrimitive().getAsLong();
+        }
+        tagCompound.setTag(propertyName, new NBTTagLongArray(longArray));
     }
 }
