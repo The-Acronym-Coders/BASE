ac75afdd03742ac6646e337d5cf443a9f2c233fd
Blaster84x <tpotancok2017@gmail.com>
2020-05-13 17:55:21 +0200

    Enable alternative namespaces
    he constructor now has an overload with an extra parameter and the direct call to `mod.getID()` is replaced. When `altNamespace` is null, the method falls back to `getID`. This makes possible for mods to optionally register entities with a different namespace, including overrides.

--- src/main/java/com/teamacronymcoders/base/registrysystem/EntityRegistry.java
+++ src/main/java/com/teamacronymcoders/base/registrysystem/EntityRegistry.java
@@ -12,9 +12,14 @@ public class EntityRegistry extends ModularRegistry<ExtendedEntityEntry> {
     public EntityRegistry(IBaseMod mod, List<IRegistryPiece> registryPieces) {
         super("ENTITY", mod, registryPieces);
     }
+    
+    public EntityRegistry(IBaseMod mod, List<IRegistryPiece> registryPieces, String altNamespace) {
+        super("ENTITY", mod, registryPieces);
+    }
 
     public void register(Class<? extends Entity> entityClass) {
-        ResourceLocation name = new ResourceLocation(this.mod.getID(), entityClass.getSimpleName());
+        String namespace = (altNamespace != null) ? altNamespace : mod.getID();
+        ResourceLocation name = new ResourceLocation(namespace, entityClass.getSimpleName());
         ExtendedEntityEntry extendedEntityEntry = new ExtendedEntityEntry(entityClass, name.toString());
         register(name, extendedEntityEntry);
     }
